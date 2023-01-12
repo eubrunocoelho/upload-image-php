a9daaa1e096c91a567387c6580e30b6cf6ba59da
eubrunocoelho <eu.brunocoelho94@gmail.com>
2023-01-12 16:41:58 -0300

    layout created

--- index.php
+++ index.php
@@ -3,21 +3,30 @@
 require 'functions/dd.php';
 require_once 'classes/Upload.php';
 
-if (!is_dir('./uploads')) mkdir('./uploads', 0777, true);
+if (!is_dir('./uploads')) {
+    mkdir('./uploads', 0777, true);
+}
 
 $filePath = dirname(__FILE__) . '/uploads/';
 
-if (isset($_FILES['uploadFile'])) {
-    $upload = new Upload($_FILES['uploadFile'], $filePath);
-    $upload->upload();
+if (
+    isset($_FILES['uploadFile'])
+) {
+    if ($_FILES['uploadFile']['name'] != '') {
+        $upload = new Upload($_FILES['uploadFile'], $filePath);
+        $upload->upload();
 
-    if ($upload->passed()) {
-        dd('Arquivo enviado com sucesso.');
+        if ($upload->passed()) {
+            $success = 'Arquivo enviado com sucesso!';
+        } else {
+            $errors = (array)$upload->errors();
+        }
     } else {
-        $errors = (array)$upload->errors();
-    };
+        $errors = (array)'Selecione um arquivo para fazer upload.';
+    }
 }
 
+$success = $success ?? null;
 $errors = $errors ?? null;
 
 ?>
@@ -30,15 +39,44 @@ $errors = $errors ?? null;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload de imagens usando PHP</title>
 </head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <body>
-    <div style="text-align: center;">
-        <h1>Upload de imagens usando PHP</h1>
-        <form action="./index.php" method="POST" enctype="multipart/form-data">
-            <label for="uploadFile">Selecione o arquivo:</label>
-            <input type="file" name="uploadFile" id="uploadFile">
-            <input type="submit" value="Upload">
-        </form>
+    <div class="mx-auto w-50 mt-5 mb-5">
+        <div class="card">
+            <div class="card-body">
+                <h2>Upload de imagens usando PHP</h2>
+                <form action="./index.php" method="POST" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="uploadFile" class="form-label">Selecione o arquivo:</label>
+                        <input type="file" name="uploadFile" id="uploadFile" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <input type="submit" value="Fazer upload" class="btn btn-primary">
+                    </div>
+                </form>
+                <?php
+                if (!is_null($success)) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?= $success ?>
+                    </div>
+                <?php
+                }
+                ?>
+                <?php
+                if (!is_null($errors)) {
+                    foreach ($errors as $error) {
+                ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $error ?>
+                        </div>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
     </div>
 </body>
 
