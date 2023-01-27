23f77d6bb55c45a8ce00a7fe58b27b81dbac19b0
Bruno Coelho <73556370+eubrunocoelho@users.noreply.github.com>
2023-01-27 09:35:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -38,8 +38,8 @@ $errors = $errors ?? null;
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload de imagens usando PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 <body>
     <div class="mx-auto w-50 mt-5 mb-5">
